a51dcec3b1d3758decfa204ec09f618a85e8b2ce
Unironic Soulizt <profdylanj@gmail.com>
2026-02-08 18:48:32 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -15,8 +15,26 @@ questions = [
             "hier": 0,
             "radc": 0
         }
-    }
+    },
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "econ": -10,
+            "prop": -5,
+            "scty": 5,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
 ];
+
 
 
 
