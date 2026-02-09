8a5c9fec2b724e55f21b5dd435b6ae4f211b5049
Unironic Soulizt <profdylanj@gmail.com>
2026-02-09 19:55:42 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,90 +1,23 @@
 questions = [
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
-            "econ": -10,
-            "prop": -5,
-            "scty": 5,
-            "cult": 0,
-            "agrs": 0,
-            "ethi": 0,
-            "mark": 0,
-            "tech": 0,
-            "dmoc": 0,
-            "ntur": 0,
-            "hier": 0,
-            "radc": 0
-        }
-    },
-    {
-        "question": "Oppression by testing is more of a concern than sleep",
-        "effect": {
-            "econ": -10,
-            "prop": -5,
-            "scty": 5,
-            "cult": 0,
-            "agrs": 0,
-            "ethi": 0,
-            "mark": 0,
-            "tech": 0,
-            "dmoc": 0,
-            "ntur": 0,
-            "hier": 0,
-            "radc": 0
-        }
-    },
-    {
-        "question": "AGRESSION TESTOR",
-        "effect": {
-            "econ": 0,
-            "prop": 0,
-            "scty": 0,
-            "cult": 0,
-            "agrs": 10,
-            "ethi": 0,
-            "mark": 0,
-            "tech": 0,
-            "dmoc": 0,
-            "ntur": 0,
-            "hier": 0,
-            "radc": 0
-        }
-    },
-    {
-        "question": "AMULTI TESTOR1",
-        "effect": {
-            "econ": 5,
-            "prop": -5,
-            "scty": 5,
-            "cult": 5,
-            "agrs": 10,
-            "ethi": -10,
-            "mark": 0,
-            "tech": 0,
-            "dmoc": 0,
-            "ntur": 0,
-            "hier": 0,
-            "radc": 0
-        }
-    },
-    {
         "question": "AMULTI TESTOR2",
         "effect": {
-            "econ": 5,
-            "prop": -5,
-            "scty": 5,
-            "cult": 5,
+            "econ": 10,
+            "prop": 10,
+            "scty": 10,
+            "cult": 10,
             "agrs": 10,
-            "ethi": -10,
-            "mark": -5,
-            "tech": 5,
-            "dmoc": 5,
+            "ethi": 10,
+            "mark": 10,
+            "tech": 10,
+            "dmoc": 10,
             "ntur": 10,
-            "hier": -10,
+            "hier": 10,
             "radc": 10
         }
     }
 ];
+
 
 
 
