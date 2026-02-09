9dfa700431ae76e29a09d8a99e8f534cc61dd3cd
Unironic Soulizt <profdylanj@gmail.com>
2026-02-09 20:20:54 +0000

    Update questions.js FIRST BATCH!

--- questions.js
+++ questions.js
@@ -1,22 +1,448 @@
 questions = [
     {
-        "question": "AMULTI TESTOR2",
+        "question": "Private property is exploitative",
+        "effect": {
+            "econ": 0,
+            "prop": 10,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Money should be abolished",
+        "effect": {
+            "econ": -10,
+            "prop": 0,
+            "scty": 5,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": -10,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Freedom of movement should be guaranteed",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 10,
+            "cult": 5,
+            "agrs": 0,
+            "ethi": 5,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Anarcho-Monarchism is preferable to Direct Democracy",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": -10,
+            "ntur": 0,
+            "hier": 5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "I subscribe to an economic school of thought",
         "effect": {
             "econ": 10,
-            "prop": 10,
-            "scty": 10,
+            "prop": 0,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Anything that should be abolished will inevitably abolish itself due to inherent contradictions",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 5,
+            "mark": 0,
+            "tech": -5,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": -5
+        }
+    },
+    {
+        "question": "The collective is all that is important",
+        "effect": {
+            "econ": 10,
+            "prop": 50,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 5,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Religion is antiquated and has no place in a modern society",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
             "cult": 10,
-            "agrs": 10,
-            "ethi": 10,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 5,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Competition drives innovation",
+        "effect": {
+            "econ": 10,
+            "prop": -10,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
             "mark": 10,
-            "tech": 10,
-            "dmoc": 10,
-            "ntur": 10,
-            "hier": 10,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "ACAB",
+        "effect": {
+            "econ": 0,
+            "prop": 10,
+            "scty": 0,
+            "cult": 5,
+            "agrs": 5,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -10,
+            "radc": 5
+        }
+    },
+    {
+        "question": "Elections are shams",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": -10,
+            "ntur": 0,
+            "hier": 0,
             "radc": 10
         }
-    }
+    },
+    {
+        "question": "The strongest in society should survive, and the weak should not",
+        "effect": {
+            "econ": 0,
+            "prop": -10,
+            "scty": 0,
+            "cult": -5,
+            "agrs": -10,
+            "ethi": 0,
+            "mark": 10,
+            "tech": 0,
+            "dmoc": -5,
+            "ntur": 0,
+            "hier": 10,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Nature has value",
+        "effect": {
+            "econ": 0,
+            "prop": 10,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 10,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Morals are spooks",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 10,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": -10,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Markets are inefficient and exploitative",
+        "effect": {
+            "econ": 0,
+            "prop": 5,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": -10,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Wars are pointless",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 10,
+            "agrs": 10,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Technology should be developed in order to curb and reverse climate change",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 10,
+            "dmoc": 0,
+            "ntur": 10,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Love is love",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 10,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Society was better 150 years ago",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": -10,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": -10,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": -10
+        }
+    },
+    {
+        "question": "Space should be colonised",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 5,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 10,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "The goal of anarchists should be the eventual abolition of all hierarchies",
+        "effect": {
+            "econ": 0,
+            "prop": 5,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -10,
+            "radc": 0
+        }
+    },
+    {
+        "question": "Democracy asks too much of people",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 10,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": -10,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "The economy is a myth",
+        "effect": {
+            "econ": -10,
+            "prop": -5,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "We should advance our biology with technology",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 10,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": 0,
+            "radc": 0
+        }
+    },
+    {
+        "question": "We are only individuals.",
+        "effect": {
+            "econ": 0,
+            "prop": 0,
+            "scty": 10,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": 0,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
+    {
+        "question": "All property is a myth",
+        "effect": {
+            "econ": -10,
+            "prop": 10,
+            "scty": 0,
+            "cult": 0,
+            "agrs": 0,
+            "ethi": 0,
+            "mark": -10,
+            "tech": 0,
+            "dmoc": 0,
+            "ntur": 0,
+            "hier": -5,
+            "radc": 0
+        }
+    },
 ];
+
 
 
 
